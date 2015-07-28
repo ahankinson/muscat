c303f445945a3866d47a4db47a743f66cef63e79
stephan <stephan@hirsch-online.com>
2015-07-28 14:22:28 +0200

    Modified xml split to fit global marc conf

--- housekeeping/scrapbook/split_file.rb
+++ housekeeping/scrapbook/split_file.rb
@@ -11,6 +11,22 @@ def change_subfield_code(node, tag, old_code, new_code)
       subfield
 end
 
+def change_datafield(node, tag, new_tag)
+      datafield=node.xpath("//datafield[@tag='#{tag}']")
+      datafield.attr('tag', new_tag) if datafield
+      datafield
+end
+
+def change_collection(node)
+      subfield=node.xpath("//datafield[@tag='100']/subfield[@code='a']")
+      if subfield.text=='Collection'
+        node.xpath("//datafield[@tag='100']").remove
+        change_datafield(node, '240', '110')
+      end
+end
+
+
+
 if ARGV.length >= 2
   source_file = ARGV[0]
   model = ARGV[1]
@@ -19,14 +35,21 @@ if ARGV.length >= 2
   ofile.write("<collection>")
   if File.exists?(source_file)
     import = MarcImport.new(source_file, model, start.to_i)
-
     import.each_record(source_file) { |record|
-
+      change_collection(record)
+      change_datafield(record, '762', '772')
+      change_datafield(record, '504', '690')
+      change_datafield(record, '510', '691')
       change_subfield_code(record,'773', 'a', 'w')
-      change_subfield_code(record,'852', 'x', '0')
-      change_subfield_code(record,'852', 'c', 'b')
 
-      ofile.write(record.to_xml :indent => 5, :encoding => 'UTF-8')
+      #Sorting tags
+      nodes = record.xpath("//datafield").remove
+      nodes.sort_by{|node| node.attr("tag")}.each{|node| record.add_child(node)}
+      doc = Nokogiri::XML.parse(record.to_s) do |config|
+        config.noblanks
+      end
+
+      ofile.write(doc.root.to_xml :encoding => 'UTF-8')
       start+=1
       if start % SIZE == 0
         ofile.write("</collection>")
@@ -37,14 +60,6 @@ if ARGV.length >= 2
       end
       #break if start==100
     }
-
-
-
-
-
-
-
-
 
 
 
